38267acca1a8b7d7400f7c3b1ae1a0e238011a41
Owen Flanagan <owen.flanagan@secretescapes.com>
2023-03-24 11:53:31 +0000

    Fix alerting

--- formative.js
+++ formative.js
@@ -1,21 +1,25 @@
-function submit() {
-   var num1 = document.calculator.firstNumber.ValueOf()
-   var num2 = document.calculator.secondNumber.ValueOf()
+function calculate() {
+   var num1 = parseInt(document.getElementById('firstNumber').value);
+   var num2 = parseInt(document.getElementById('secondNumber').value);
    var equation = document.calculator.equation.value
       
    if (equation == "plus") {
-      return alert(num1 + num2);
+      var result = num1 + num2;
+      alert("The result is " + result);
    }
 
    if (equation == "minus") {
-      return alert(num1 - num2);
+      var result = num1 - num2;
+      alert("The result is " + result);
    }
 
    if (equation == "multiply") {
-      return alert(num1 * num2);
+      var result =  num1 * num2;
+      alert("The result is " + result);
    }
 
    if (equation == "divide") {
-      return alert(num1 / num2);
+      var result =  num1 / num2;
+      alert("The result is " + result);
    }
 }
